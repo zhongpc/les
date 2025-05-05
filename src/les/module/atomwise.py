bbe3dbbaffb559e1a5f61e68e202b138dbd890b2
djkim-kr <dongjin.kim@berkeley.edu>
2025-05-05 11:19:45 -0700

    temp commit, Dense problem unsolved

--- src/les/module/atomwise.py
+++ src/les/module/atomwise.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union, Sequence, Callable, Optional
+from typing import Dict, Union, Sequence, Callable, Optional, List
 
 import torch
 import torch.nn as nn
@@ -16,7 +16,7 @@ class Atomwise(nn.Module):
     def __init__(
         self,
         n_out: int = 1,
-        n_hidden: Optional[Union[int, Sequence[int]]] = None,
+        n_hidden: Optional[List[int]] = None,
         n_layers: int = 2,
         bias: bool = True,
         activation: Callable = F.silu,
@@ -37,7 +37,7 @@ class Atomwise(nn.Module):
         """
         super().__init__()
 
-        self.n_in = None
+        self.n_in: int = 1
         self.n_out = n_out
         self.n_hidden = n_hidden
         self.n_layers = n_layers
@@ -45,39 +45,73 @@ class Atomwise(nn.Module):
         self.add_linear_nn = add_linear_nn
         self.bias = bias
         self.output_scaling_factor = output_scaling_factor
-        self.outnet = None
+        self.outnet = nn.Identity()
+        # self._init_networks()
+
+    def _init_networks(self):
+        self.outnet = build_mlp(
+            n_in=self.n_in,
+            n_out=self.n_out,
+            n_hidden=self.n_hidden,
+            n_layers=self.n_layers,
+            activation=self.activation,
+            bias=self.bias,
+        )
+        if self.add_linear_nn:
+            self.linear_nn = Dense(
+                self.n_in,
+                self.n_out,
+                bias=self.bias,
+                activation=None,
+            )
+        else:
+            self.linear_nn = None
 
     def forward(self, 
                 desc: torch.Tensor, # [n_atoms, n_features]
                 batch: torch.Tensor, # [n_atoms]
                 training: bool = None,
                ) -> torch.Tensor:
-
-        if self.n_in is None:
-            self.n_in = desc.shape[1]
-        else:
-            assert self.n_in == desc.shape[1]
-
-        if self.outnet is None:
-            self.outnet = build_mlp(
-                n_in=self.n_in,
-                n_out=self.n_out,
-                n_hidden=self.n_hidden,
-                n_layers=self.n_layers,
-                activation=self.activation,
-                bias=self.bias,
-                )
-            self.outnet = self.outnet.to(desc.device)
-            if self.add_linear_nn:
-                self.linear_nn = Dense(
-                   self.n_in,
-                   self.n_out,
-                   bias=self.bias,
-                   activation=None,
-                   )
-                self.linear_nn = self.linear_nn.to(desc.device)
+        if desc.shape[1] != self.n_in:
+            if self.n_in == 1:
+                self.n_in = desc.shape[1]
+                self._init_networks()
+                self.outnet = self.outnet.to(desc.device)
+                if self.add_linear_nn:
+                    self.linear_nn = self.linear_nn.to(desc.device)
             else:
-                self.linear_nn = None
+                raise ValueError(f"Input dimension {desc.shape[1]} does not match expected dimension {self.n_in}.")                    
+
+        # if self.n_in == 1:
+        #     self.n_in = desc.shape[1]
+        # else:
+        #     assert self.n_in == desc.shape[1]
+        
+        # self._init_networks()
+        # self.outnet = self.outnet.to(desc.device)
+        # if self.add_linear_nn:
+        #     self.linear_nn = self.linear_nn.to(desc.device)
+
+        # if self.outnet is None:
+        #     self.outnet = build_mlp(
+        #         n_in=self.n_in,
+        #         n_out=self.n_out,
+        #         n_hidden=self.n_hidden,
+        #         n_layers=self.n_layers,
+        #         activation=self.activation,
+        #         bias=self.bias,
+        #         )
+        #     self.outnet = self.outnet.to(desc.device)
+        #     if self.add_linear_nn:
+        #         self.linear_nn = Dense(
+        #            self.n_in,
+        #            self.n_out,
+        #            bias=self.bias,
+        #            activation=None,
+        #            )
+        #         self.linear_nn = self.linear_nn.to(desc.device)
+        #     else:
+        #         self.linear_nn = None
 
         # predict atomwise contributions
         y = self.outnet(desc)
